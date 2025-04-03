ee1afe023f077d13832c4af7c6a47790977f7f74
Shanks <shashank.attarde@juspay.in>
2025-04-03 15:28:03 +0530

    refactor(project): adopt a lint family structure for lints (#1)

---
+++ use_common_utils/src/lib.rs
@@ -0,0 +1,13 @@
+#![feature(rustc_private)]
+#![warn(unused_extern_crates)]
+
+dylint_linting::dylint_library!();
+
+extern crate rustc_lint;
+extern crate rustc_session;
+
+#[expect(clippy::no_mangle_with_rust_abi)]
+#[no_mangle]
+pub fn register_lints(sess: &rustc_session::Session, lint_store: &mut rustc_lint::LintStore) {
+    use_common_serde_utils::register_lints(sess, lint_store);
+}
